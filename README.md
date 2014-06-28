97e73453e0b541e19a57c6708ea68df42a4a06b3
root <root@debian.debian>
2014-06-29 00:19:15 +0200

    [+] add files to repository

---
+++ README.md
@@ -0,0 +1,14 @@
+CoolPhoto client photogallery module
+
+Author:
+Veriaiskiy Fedor
+
+Date:
+2014-5-5
+
+Description:
+client photogallery module, allows to watch a fullsize photogallery images and dynamically navigate through them.
+Fullsize image popup also represents information about current photo showed and automatically adjusts itself size to a browser window size.
+
+Application implementation example:
+http://www.kidsreview.ru/spb/photogallery/kontsertnyi-zal-dom-kochnevoi-v-sankt-peterburge-foto#
---
+++ mod-coolPhoto/module-usage-example.tpl.php
@@ -0,0 +1,25 @@
+<!-- coolPhoto galley plugin usage example -->
+<!-- ... -->
+<ul class="kidsreview-photogallery">
+    <li>
+        <a href="/images/photos_events/restoran_moskva_nevskiy_centr_foto_079.jpg" rel="coolPhoto[gallery1]">
+            <img src="/images/photos_events/restoran_moskva_nevskiy_centr_foto_079.jpg"
+                 alt="Москва - ресторан для всей семьи с детской комнатой, фото"/>
+        </a>
+    </li>
+
+    <li>
+        <a href="/images/photos_events/restoran_moskva_nevskiy_centr_foto_082.jpg" rel="coolPhoto[gallery1]">
+            <img src="/images/photos_events/restoran_moskva_nevskiy_centr_foto_082.jpg"
+                 alt="Москва - ресторан для всей семьи с детской комнатой, фото"/>
+        </a>
+    </li>
+
+    <li>
+        <a href="/images/photos_events/restoran_moskva_nevskiy_centr_foto_003.jpg" rel="coolPhoto[gallery1]">
+            <img src="/images/photos_events/restoran_moskva_nevskiy_centr_foto_003.jpg"
+                 alt="Москва - ресторан для всей семьи с детской комнатой, фото"/>
+        </a>
+    </li>
+</ul>
+<!-- ... -->
